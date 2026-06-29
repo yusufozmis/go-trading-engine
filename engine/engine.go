1ee666f1fd4831bc4f648fb0914f06c13b59e402
Yusuf <yusufozmis2@gmail.com>
2026-06-29 19:44:18 +0300

    feat: introduce engine package to manage low-level functionality

---
+++ engine/engine.go
@@ -0,0 +1,29 @@
+package engine
+
+import (
+	"github.com/yusufozmis/trading-library/types"
+)
+
+type Engine struct {
+	Symbol    string
+	Timeframe string
+
+	isCloseAutomated bool
+	LastPosition     *types.Positions
+
+	ClosedPositions []types.Positions
+
+	lockKeyMap  map[string]bool
+	activePlans []types.EntryPlan
+
+	pendingConfirmation *types.EntryPlan
+}
+
+func NewEngine(symbol, timeframe string, isCloseAutomated bool) *Engine {
+	return &Engine{
+		Symbol:           symbol,
+		Timeframe:        timeframe,
+		isCloseAutomated: isCloseAutomated,
+		lockKeyMap:       make(map[string]bool),
+	}
+}
